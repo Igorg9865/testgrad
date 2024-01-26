665aa49f2dca19f4aec8569b052e23c67f36ed11
Igorg9865 <igor@DESKTOP-GPBP2TO>
2024-01-26 21:56:43 +0100

    fixed Dict bug and added Union

--- tgrad/llops.py
+++ tgrad/llops.py
@@ -1,6 +1,6 @@
 import numpy as np
 from enum import Enum, auto
-# from typing import Union  --> why would you use Union[x, y] vs (x | y) ??
+from typing import Union, Dict, Callable  
 
 class UnaryOps(Enum): NOOP = auto(); EXP2 = auto(); LOG2 = auto(); SIN = auto(); SQRT = auto(); CAST = auto(); NEG = auto()
 class BinaryOps(Enum): ADD = auto(); SUB = auto(); MUL = auto(); DIV = auto(); CMPEQ = auto(); CMPLT = auto(); MOD = auto(); MAX = auto(); XOR = auto()
@@ -9,9 +9,9 @@ class ReduceOps(Enum): SUM = auto(); MAX = auto()
 class MovementOps(Enum): pass
 class LoadOps(Enum): pass
 
-Ops = (UnaryOps | BinaryOps | ReduceOps)
+Ops = Union[UnaryOps, BinaryOps, ReduceOps]
 
-op_fxn: dict(Ops) = {
+op_fxn: Dict[Ops, Callable] = {
     UnaryOps.EXP2: np.exp2, UnaryOps.LOG2: np.log2, UnaryOps.SIN: np.sin, UnaryOps.NEG: np.negative, ## Add UnaryOps.cast later
     BinaryOps.ADD: np.add, BinaryOps.SUB: np.subtract, BinaryOps.MUL: np.multiply, BinaryOps.DIV: np.divide, 
     BinaryOps.CMPEQ: np.equal, BinaryOps.CMPLT: np.less, BinaryOps.MOD: np.mod, BinaryOps.MAX: np.maximum, BinaryOps.XOR: np.bitwise_xor,
