fda7a5124056ac8c50e130102e0b35b3a5d4ff08
Igorg9865 <igor@DESKTOP-GPBP2TO>
2024-01-26 11:27:13 +0100

    added unaryops, binaryops and reduce ops

--- tgrad/llops.py
+++ tgrad/llops.py
@@ -1,1 +1,6 @@
 import numpy as np
+from enum import Enum, auto
+
+class UnaryOps(Enum): NOOP = auto(); EXP2 = auto(); LOG2 = auto(); SIN = auto(); SQRT = auto(); CAST = auto(); NEG = auto()
+class BinaryOps(Enum): ADD = auto(); SUB = auto(); MUL = auto(); DIV = auto(); CMPEQ = auto(); CMPLT = auto(); MOD = auto(); MAX = auto(); XOR = auto()
+class ReduceOps(Enum): SUM = auto(); MAX = auto()
